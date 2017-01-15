eae2705adff78f3c46b53ba23357ffcc10a022a8
just nobody <kingdaro@gmail.com>
2017-01-15 16:07:22 -0500

    split up some files

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,7 @@
 import {Color} from './color'
 import {Point} from './point'
+import {Note, NoteHitAnimation} from './note'
+import {Judgement, JudgementAnimation} from './judgement'
 import * as graphics from './graphics'
 import * as util from './util'
 
@@ -10,120 +12,20 @@ export const viewHeight = 1280
 export const viewWidth = 720
 export const receptorPosition = viewHeight - 210
 
-enum NoteState {
-  active,
-  hit,
-  missed,
-  holding,
-}
-
-enum NoteJudgement {
-  absolute,
-  perfect,
-  great,
-  bad,
-  none,
-}
-
-interface Drawable {
+export interface Drawable {
   draw(c: CanvasRenderingContext2D): void
 }
 
-interface Animation extends Drawable {
+export interface Animation extends Drawable {
   time: number
   update(dt: number): this
   isFinished(): boolean
 }
 
-class Note implements Drawable {
-  state = NoteState.active
-
-  constructor(public time: number, public position: number) {}
-
-  getScreenPosition(): Point {
-    const x = util.lerp(trackLeft, viewWidth - trackRight, this.position)
-    const y = util.lerp(receptorPosition, receptorPosition - noteSpacing, this.time)
-    return new Point(x, y)
-  }
-
-  draw(c: CanvasRenderingContext2D) {
-    const pos = this.getScreenPosition()
-    graphics.applyCenteredRotation(45, pos, c, () => {
-      graphics.rectangle(c, -25, -25, 50).fill(Color.white)
-      graphics.rectangle(c, -30, -30, 60).stroke(Color.white.fade(0.7), 2)
-    })
-  }
-}
-
-class NoteHitAnimation implements Animation {
-  time = 0
-
-  constructor(public pos: Point) {}
-
-  update(dt: number): this {
-    this.time += dt * 2
-    return this
-  }
-
-  isFinished(): boolean {
-    return this.time >= 1
-  }
-
-  draw(c: CanvasRenderingContext2D) {
-    const drift = this.time ** 2 * 200
-    const opacity = 1 - util.clamp(this.time, 0, 1) ** 1.7
-    const glowAmount = (1 - this.time ** 2) * 75
-
-    const pos = this.pos.add(new Point(0, drift))
-    const glowColor = Color.white.fade(opacity)
-
-    graphics.applyShadow(c, glowColor, glowAmount, 0, 0, () => {
-      graphics.applyCenteredRotation(45, pos, c, () => {
-        graphics.rectangle(c, -30, -30, 60).fill(glowColor)
-      })
-    })
-  }
-}
-
-class JudgementAnimation implements Animation {
-  time = 0
-
-  constructor(public judgement: NoteJudgement) {}
-
-  update(dt: number) {
-    this.time += dt
-    return this
-  }
-
-  isFinished() {
-    return this.time >= 1.3
-  }
-
-  draw(c: CanvasRenderingContext2D) {
-    const text =
-      this.judgement === NoteJudgement.absolute ? 'ABSOLUTE' :
-      this.judgement === NoteJudgement.perfect ? 'PERFECT' :
-      this.judgement === NoteJudgement.great ? 'GREAT' :
-      this.judgement === NoteJudgement.bad ? 'BAD' :
-      ''
-
-    const offset = (1 - util.clamp(this.time * 4, 0, 1) ** 0.5) * 32
-    const opacity = 1 - util.delta(this.time, 1, 1.3)
-    const judgeColor = Color.white.fade(util.clamp(opacity, 0, 1))
-
-    graphics.applyShadow(c, judgeColor.fade(0.3), 20, 0, 0, () => {
-      c.font = '80px Roboto Condensed'
-      c.textAlign = 'center'
-      c.fillStyle = judgeColor.toString()
-      c.fillText(text, viewWidth / 2, viewHeight * 0.3 + offset)
-    })
-  }
-}
-
 export class Game {
   notes = [] as Note[]
   animations = [] as Animation[]
-  judgement = new JudgementAnimation(NoteJudgement.none)
+  judgement = new JudgementAnimation(Judgement.none)
   songTime = -2
 
   constructor() {
@@ -158,7 +60,7 @@ export class Game {
 
       if (tapDistance < 100) {
         const judgement = this.getJudgement(timing)
-        if (judgement < NoteJudgement.none) {
+        if (judgement < Judgement.none) {
           this.notes.splice(i, 1)
           this.animations.push(new NoteHitAnimation(new Point(pos, receptorPosition)))
           this.judgement = new JudgementAnimation(judgement)
@@ -170,11 +72,11 @@ export class Game {
 
   getJudgement(timing: number) {
     return (
-      timing <= 0.015 ? NoteJudgement.absolute :
-      timing <= 0.08 ? NoteJudgement.perfect :
-      timing <= 0.12 ? NoteJudgement.great :
-      timing <= 0.2 ? NoteJudgement.bad :
-      NoteJudgement.none
+      timing <= 0.015 ? Judgement.absolute :
+      timing <= 0.08 ? Judgement.perfect :
+      timing <= 0.12 ? Judgement.great :
+      timing <= 0.2 ? Judgement.bad :
+      Judgement.none
     )
   }
 
---
+++ src/note.ts
@@ -0,0 +1,63 @@
+import {Point} from './point'
+import {Drawable, Animation} from './game'
+import {Color} from './color'
+import * as game from './game'
+import * as graphics from './graphics'
+import * as util from './util'
+
+export enum NoteState {
+  active,
+  hit,
+  missed,
+  holding,
+}
+
+export class Note implements Drawable {
+  state = NoteState.active
+
+  constructor(public time: number, public position: number) {}
+
+  getScreenPosition(): Point {
+    const x = util.lerp(game.trackLeft, game.viewWidth - game.trackRight, this.position)
+    const y = util.lerp(game.receptorPosition, game.receptorPosition - game.noteSpacing, this.time)
+    return new Point(x, y)
+  }
+
+  draw(c: CanvasRenderingContext2D) {
+    const pos = this.getScreenPosition()
+    graphics.applyCenteredRotation(45, pos, c, () => {
+      graphics.rectangle(c, -25, -25, 50).fill(Color.white)
+      graphics.rectangle(c, -30, -30, 60).stroke(Color.white.fade(0.7), 2)
+    })
+  }
+}
+
+export class NoteHitAnimation implements Animation {
+  time = 0
+
+  constructor(public pos: Point) {}
+
+  update(dt: number): this {
+    this.time += dt * 2
+    return this
+  }
+
+  isFinished(): boolean {
+    return this.time >= 1
+  }
+
+  draw(c: CanvasRenderingContext2D) {
+    const drift = this.time ** 2 * 200
+    const opacity = 1 - util.clamp(this.time, 0, 1) ** 1.7
+    const glowAmount = (1 - this.time ** 2) * 75
+
+    const pos = this.pos.add(new Point(0, drift))
+    const glowColor = Color.white.fade(opacity)
+
+    graphics.applyShadow(c, glowColor, glowAmount, 0, 0, () => {
+      graphics.applyCenteredRotation(45, pos, c, () => {
+        graphics.rectangle(c, -30, -30, 60).fill(glowColor)
+      })
+    })
+  }
+}
