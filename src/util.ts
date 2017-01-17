81550a90ae38beb2b45f3d94e7879c4992f3970c
just nobody <kingdaro@gmail.com>
2017-01-17 15:45:10 -0500

    some more util functions

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,11 @@
 export function lerp(a: number, b: number, delta: number) {
   return a + (b - a) * delta
 }
+
+export function delta(num: number, a: number, b: number) {
+  return b - a === 0 ? 0 : (num - a) / (b - a)
+}
+
+export function radians(degrees: number) {
+  return degrees / 180 * Math.PI
+}
