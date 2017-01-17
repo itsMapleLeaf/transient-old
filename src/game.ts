cb0ad8cd06ee3634d609de0c1a2aca98d785c798
just nobody <kingdaro@gmail.com>
2017-01-17 15:07:45 -0500

    probably shouldn't try to extend pixi objects...

--- src/game.ts
+++ src/game.ts
@@ -4,22 +4,19 @@ import {createRectangle} from './rectangle'
 export const viewWidth = 540
 export const viewHeight = 960
 
-class Note extends pixi.Container {
-  constructor() {
-    super()
-
-    const inner = this.addChild(createRectangle(0xffffff, 0, 0, 40).fill())
-    const outer = this.addChild(createRectangle(0xffffff, 0, 0, 50).stroke(1))
-
-    this.rotation = Math.PI * 0.25
-  }
+function createNote() {
+  const note = new pixi.Container()
+  note.addChild(createRectangle(0xffffff, 0, 0, 40).fill())
+  note.addChild(createRectangle(0xffffff, 0, 0, 50).stroke(1))
+  note.rotation = Math.PI * 0.25
+  return note
 }
 
 export class Game {
   stage = new pixi.Container()
 
   constructor(public renderer: pixi.WebGLRenderer | pixi.CanvasRenderer) {
-    const note = this.stage.addChild(new Note())
+    const note = this.stage.addChild(createNote())
     note.position.set(100, 100)
   }
 
