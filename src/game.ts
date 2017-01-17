86b2a07a6122660fd2514b9c787d02046a34a936
just nobody <kingdaro@gmail.com>
2017-01-17 15:38:21 -0500

    comments because it's not easy to know what something is just by looking at the numbers used to make it

--- src/game.ts
+++ src/game.ts
@@ -12,7 +12,10 @@ class Note {
   sprite = new pixi.Container()
 
   constructor(public time: number, public position: number) {
+    // inner square
     this.sprite.addChild(createRect(0, 0, 40).fill(0xffffff))
+
+    // outer square
     this.sprite.addChild(createRect(0, 0, 50).stroke(1, 0xffffff))
 
     this.sprite.position.x = util.lerp(trackMargin, viewWidth - trackMargin, position)
@@ -29,15 +32,20 @@ export class Game {
   songTime = -2
 
   constructor(public renderer: pixi.WebGLRenderer | pixi.CanvasRenderer) {
+    // test notes
     this.addNote(0 / 2, 0 / 4)
     this.addNote(1 / 2, 1 / 4)
     this.addNote(2 / 2, 2 / 4)
     this.addNote(3 / 2, 3 / 4)
     this.addNote(4 / 2, 4 / 4)
 
+    // receptor
     this.stage.addChild(createRect(viewWidth / 2, receptorPosition, viewWidth, 10).fill(0xffffff, 0.5))
+
+    // note container
     this.stage.addChild(this.noteContainer)
 
+    // events
     this.input.on('pointerdown', this.pointerdown, this)
   }
 
