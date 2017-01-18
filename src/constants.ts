037f67a1f03598fcf5e497b34678d5b6b765a86d
just nobody <kingdaro@gmail.com>
2017-01-18 16:47:44 -0500

    added some comments to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,12 @@
+// game viewport dimensions
 export const viewWidth = 540
 export const viewHeight = 960
-export const noteSpacing = 300 // pixels per second
+
+// the distance between notes in pixels per second at speed 1
+export const noteSpacing = 300
+
+// the distance from the center of notes to the edge of the screen
 export const trackMargin = 100
+
+// vertical position of the receptor
 export const receptorPosition = viewHeight * 0.88
