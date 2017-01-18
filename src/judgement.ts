102f6ea1a25857362ec42b5471ab8935758163e7
just nobody <kingdaro@gmail.com>
2017-01-18 13:08:51 -0500

    split out judgement logic

---
+++ src/judgement.ts
@@ -0,0 +1,61 @@
+import * as pixi from 'pixi.js'
+import * as util from './util'
+
+enum Judgement {
+  absolute,
+  perfect,
+  great,
+  bad,
+  none,
+}
+
+const judgementText = {
+  [Judgement.absolute]: 'ABSOLUTE',
+  [Judgement.perfect]: 'PERFECT',
+  [Judgement.great]: 'GREAT',
+  [Judgement.bad]: 'BAD',
+}
+
+export const timingWindow = {
+  [Judgement.absolute]: 0.02,
+  [Judgement.perfect]: 0.08,
+  [Judgement.great]: 0.15,
+  [Judgement.bad]: 0.28,
+}
+
+export function getJudgement(timing: number) {
+  for (let i = 0; i < Judgement.none; i++) {
+    if (Math.abs(timing) < timingWindow[i]) {
+      return i as Judgement
+    }
+  }
+  return Judgement.none
+}
+
+export class JudgementAnimation extends pixi.Container {
+  text = this.addChild(new pixi.Text('ABSOLUTE', {
+    fill: 0xffffff,
+    fontSize: 80
+  }))
+
+  judgement = Judgement.none
+  time = 0
+
+  constructor(x: number, y: number) {
+    super()
+    this.position.set(x, y)
+  }
+
+  update(dt: number) {
+    this.time += dt
+    this.text.y = util.lerp(20, 0, util.clamp((this.time / 0.25) ** 0.5, 0, 1))
+    this.text.text = judgementText[this.judgement] || ''
+    this.text.pivot.x = this.text.width / 2
+    this.alpha = util.lerp(1, 0, util.clamp(util.delta(this.time, 0.8, 1.0), 0, 1))
+  }
+
+  play(judgement: Judgement) {
+    this.time = 0
+    this.judgement = judgement
+  }
+}
