de6e7e53c90fe726059e634ecc409bb9c43d3192
just nobody <kingdaro@gmail.com>
2017-01-22 18:48:07 -0500

    cleaned up main

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,58 @@
 import * as pixi from 'pixi.js'
+import * as WebFontLoader from 'webfontloader'
 import Game, {viewWidth, viewHeight} from './game'
 
-function startGame() {
-  const canvas = document.querySelector('canvas') as HTMLCanvasElement
-
-  const renderer = pixi.autoDetectRenderer(viewWidth, viewHeight, { view: canvas })
-  renderer.clearBeforeRender = false
-
-  const interaction = new pixi.interaction.InteractionManager(renderer)
-
-  const game = new Game()
-  let time = 0
-
-  window.requestAnimationFrame(function gameloop(now) {
-    const dt = (now - time) / 1000
-    time = now
-    game.update(dt)
-    game.draw(renderer)
-    window.requestAnimationFrame(gameloop)
+function animationFrame(): Promise<number> {
+  return new Promise((resolve, reject) => {
+    window.requestAnimationFrame(resolve)
   })
-
-  interaction.on('pointerdown', game.pointerdown.bind(game))
 }
 
-function loadResources() {
+function loadImages() {
   pixi.loader.add('note', require('../assets/images/note.svg'))
   pixi.loader.add('background', require('../assets/images/background.svg'))
   pixi.loader.add('receptor', require('../assets/images/receptor.svg'))
   pixi.loader.add('explosion', require('../assets/images/explosion.svg'))
-  pixi.loader.load(startGame).on('error', err => console.error(err))
+
+  return new Promise((resolve, reject) => {
+    pixi.loader.load(resolve).on('error', reject)
+  })
 }
 
-loadResources()
+function loadFonts() {
+  return new Promise((resolve, reject) => {
+    WebFontLoader.load({
+      google: {
+        families: ['Teko']
+      },
+      active: resolve,
+      inactive: reject,
+    })
+  })
+}
+
+async function startGame() {
+  const canvas = document.querySelector('canvas') as HTMLCanvasElement
+  const renderer = pixi.autoDetectRenderer(viewWidth, viewHeight, { view: canvas })
+  const game = new Game()
+
+  const interaction = new pixi.interaction.InteractionManager(renderer)
+  interaction.on('pointerdown', game.pointerdown.bind(game))
+
+  let time = await animationFrame()
+  while (true) {
+    const now = await animationFrame()
+    const dt = (now - time) / 1000
+    time = now
+    game.update(dt)
+    game.draw(renderer)
+  }
+}
+
+async function main() {
+  await loadImages()
+  await loadFonts()
+  startGame()
+}
+
+main().catch(err => console.error(err))
