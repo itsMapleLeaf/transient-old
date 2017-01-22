93f16cda6158c71668d02950ae7b4c5747159082
just nobody <kingdaro@gmail.com>
2017-01-22 08:16:54 -0500

    dunno how this works but cool

--- src/game.ts
+++ src/game.ts
@@ -28,11 +28,11 @@ class Song {
 class Note {
   state = NoteState.active
 
-  constructor(public time: number, public position: number) {}
+  constructor(public time: number, public position: number, public song: Song) {}
 
   get screenPosition() {
     const x = util.lerp(110, viewWidth - 110, this.position)
-    const y = -this.time * noteSpacing
+    const y = receptorPosition - (this.time - this.song.time) * noteSpacing
     return new pixi.Point(x, y)
   }
 
@@ -82,31 +82,21 @@ export default class Game {
   notes = [] as Note[]
   explosions = [] as NoteExplosion[]
 
-  stage = new pixi.Container()
-  noteContainer = new pixi.Container()
-  receptorContainer = new pixi.Container()
-  explosionContainer = new pixi.Container()
-
   constructor() {
     this.notes = [
-      new Note(0 / 2, 0 / 4),
-      new Note(1 / 2, 1 / 4),
-      new Note(2 / 2, 2 / 4),
-      new Note(3 / 2, 3 / 4),
-      new Note(4 / 2, 4 / 4),
+      new Note(0 / 2, 0 / 4, this.song),
+      new Note(1 / 2, 1 / 4, this.song),
+      new Note(2 / 2, 2 / 4, this.song),
+      new Note(3 / 2, 3 / 4, this.song),
+      new Note(4 / 2, 4 / 4, this.song),
     ]
-
-    this.stage.addChild(renderBackground())
-    this.stage.addChild(this.receptorContainer)
-    this.stage.addChild(this.explosionContainer)
-    this.stage.addChild(this.noteContainer)
   }
 
   update(dt: number) {
     this.song.time += dt
 
     // update note positions
-    this.noteContainer.y = receptorPosition + this.song.time * noteSpacing
+    // this.noteContainer.y = receptorPosition + this.song.time * noteSpacing
 
     // update note explosion animations, only keep ones that are still active
     this.explosions = this.explosions.filter(anim => anim.update(dt))
@@ -120,34 +110,33 @@ export default class Game {
   }
 
   draw(renderer: pixi.SystemRenderer) {
-    this.renderNotes()
-    this.renderReceptors()
-    this.renderExplosions()
-    renderer.render(this.stage)
+    renderer.clearBeforeRender = false
+    renderer.render(renderBackground())
+    this.renderReceptors(renderer)
+    this.renderExplosions(renderer)
+    this.renderNotes(renderer)
+    // renderer.render(this.stage)
   }
 
-  renderNotes() {
-    this.noteContainer.removeChildren()
+  renderNotes(renderer: pixi.SystemRenderer) {
     this.notes
       .filter(note => note.state === NoteState.active)
       .map(note => note.render())
-      .forEach(sprite => this.noteContainer.addChild(sprite))
+      .forEach(sprite => renderer.render(sprite))
   }
 
-  renderReceptors() {
-    this.receptorContainer.removeChildren()
+  renderReceptors(renderer: pixi.SystemRenderer) {
     this.notes
       .filter(note => note.state === NoteState.active)
       .filter(note => this.song.time < note.time)
       .map(note => new Receptor(note, this.song.time).render())
-      .forEach(sprite => this.receptorContainer.addChild(sprite))
+      .forEach(sprite => renderer.render(sprite))
   }
 
-  renderExplosions() {
-    this.explosionContainer.removeChildren()
+  renderExplosions(renderer: pixi.SystemRenderer) {
     this.explosions
       .map(exp => exp.render())
-      .forEach(sprite => this.explosionContainer.addChild(sprite))
+      .forEach(sprite => renderer.render(sprite))
   }
 
   tryTapNote(touch: pixi.Point) {
