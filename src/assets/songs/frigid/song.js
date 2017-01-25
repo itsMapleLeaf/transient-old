ea2f08b46dacfc0d53fb8e5607d6b1c02a5108c1
just nobody <kingdaro@gmail.com>
2017-01-25 05:30:20 -0500

    use JS for song format (for now)

---
+++ src/assets/songs/frigid/song.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Frigid',
+  artist: 'just nobody',
+  art: require('./v221.png'),
+  offset: 1.317,
+  audio: [
+    require('./just nobody - Frigid.ogg'),
+    require('./just nobody - Frigid.mp3'),
+  ],
+  notes: [
+    [0.0, 0.00],
+    [0.0, 0.50],
+    [0.0, 1.00],
+  ]
+}
